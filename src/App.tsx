334a3cefdb17f2f06b1a89eb0320b1faadf7e512
sijinyu <sijinyudev@gmail.com>
2023-03-04 00:12:08 +0900

    feat: 한글로 입력시 영어 자동변환

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,90 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { convertToEnglish, koreanToEnglishMap } from "./assets/helper";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
+  const isShiftKeyPressed = useRef(false);
+
+  function handleChangeEmail(value: string): void {
+    setEmail(value);
+  }
+  function handleKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+      isShiftKeyPressed.current = false;
+    }
+  }
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+      isShiftKeyPressed.current = true;
+    }
+  }
+  function handleInput(
+    inputEvent: React.ChangeEvent<HTMLInputElement>,
+    callback: (value: string) => void
+  ) {
+    const inputValue = inputEvent.target.value;
+    const jamoArray = splitJamo(inputValue);
+    const convertedText = jamoArray
+      .map((jamo) =>
+        convertToEnglish(koreanToEnglishMap, isShiftKeyPressed.current)(jamo)
+      )
+      .join("");
+    callback(convertedText);
+  }
+
+  function splitJamo(input: string) {
+    const hangulPattern = /[\uAC00-\uD7AF]/;
+    const splitPattern = /([\u1100-\u1112\u1161-\u1175\u11A8-\u11C2])/;
+
+    const jamoArray = [];
+    let currentSyllable = "";
+    let lastCharWasHangul = false;
+
+    for (let i = 0; i < input.length; i++) {
+      const char = input.charAt(i);
+      if (hangulPattern.test(char)) {
+        if (lastCharWasHangul) {
+          jamoArray.push(...currentSyllable.split(""));
+          currentSyllable = "";
+        }
+        currentSyllable += char;
+        lastCharWasHangul = true;
+      } else if (splitPattern.test(char)) {
+        if (lastCharWasHangul) {
+          jamoArray.push(...currentSyllable.split(""));
+          currentSyllable = "";
+        }
+        jamoArray.push(char);
+        lastCharWasHangul = false;
+      } else {
+        if (lastCharWasHangul) {
+          jamoArray.push(...currentSyllable.split(""));
+          currentSyllable = "";
+        }
+        jamoArray.push(char);
+        lastCharWasHangul = false;
+      }
+    }
+
+    if (lastCharWasHangul) {
+      jamoArray.push(...currentSyllable.split(""));
+    }
+
+    return jamoArray;
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        value={email}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+        onChange={(e) => handleInput(e, handleChangeEmail)}
+      />
     </div>
   );
 }
---
+++ src/assets/helper/index.ts
@@ -0,0 +1,66 @@
+export {};
+interface CharMap {
+  [key: string]: { lower: string; upper: string };
+}
+
+export const koreanToEnglishMap: CharMap = {
+  ㄱ: { lower: "r", upper: "R" },
+  ㄲ: { lower: "R", upper: "R" },
+  ㄴ: { lower: "s", upper: "S" },
+  ㄷ: { lower: "e", upper: "E" },
+  ㄸ: { lower: "E", upper: "E" },
+  ㄹ: { lower: "f", upper: "F" },
+  ㅁ: { lower: "a", upper: "A" },
+  ㅂ: { lower: "q", upper: "Q" },
+  ㅃ: { lower: "Q", upper: "Q" },
+  ㅅ: { lower: "t", upper: "T" },
+  ㅆ: { lower: "T", upper: "T" },
+  ㅇ: { lower: "d", upper: "D" },
+  ㅈ: { lower: "w", upper: "W" },
+  ㅉ: { lower: "W", upper: "W" },
+  ㅊ: { lower: "c", upper: "C" },
+  ㅋ: { lower: "z", upper: "Z" },
+  ㅌ: { lower: "x", upper: "X" },
+  ㅍ: { lower: "v", upper: "V" },
+  ㅎ: { lower: "g", upper: "G" },
+  ㅏ: { lower: "k", upper: "K" },
+  ㅐ: { lower: "o", upper: "O" },
+  ㅑ: { lower: "i", upper: "I" },
+  ㅒ: { lower: "O", upper: "O" },
+  ㅓ: { lower: "j", upper: "J" },
+  ㅔ: { lower: "p", upper: "P" },
+  ㅕ: { lower: "u", upper: "U" },
+  ㅖ: { lower: "P", upper: "P" },
+  ㅗ: { lower: "h", upper: "H" },
+  ㅘ: { lower: "K", upper: "K" },
+  ㅙ: { lower: "O", upper: "O" },
+  ㅚ: { lower: "l", upper: "L" },
+  ㅛ: { lower: "y", upper: "Y" },
+  ㅜ: { lower: "n", upper: "N" },
+  ㅝ: { lower: "J", upper: "J" },
+  ㅞ: { lower: "U", upper: "U" },
+  ㅟ: { lower: "N", upper: "N" },
+  ㅠ: { lower: "b", upper: "B" },
+  ㅡ: { lower: "m", upper: "M" },
+  ㅢ: { lower: "M", upper: "M" },
+  ㅣ: { lower: "l", upper: "L" },
+};
+
+export function convertToEnglish(charMap: CharMap, isShiftKeyPressed: boolean) {
+  let shouldCapitalize = false;
+
+  return function (char: string) {
+    if (charMap[char]) {
+      const englishChar = charMap[char];
+
+      if (isShiftKeyPressed && !shouldCapitalize) {
+        shouldCapitalize = true;
+        return englishChar.upper;
+      } else {
+        shouldCapitalize = false;
+        return englishChar.lower;
+      }
+    }
+    return char;
+  };
+}
